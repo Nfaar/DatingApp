ea0d468e611b2aac340a120a2a649426c62ac713
Naydenov,Viktor V <v.naydenov@student.fontys.nl>
2021-01-24 15:54:49 +0100

    first push

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<AppUser> Users { get; set; }
+
+    }
+}
